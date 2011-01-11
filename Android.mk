c3d54823d45c6fcbe61f70831d0efd433d43db37
Mostafa Hajizadeh <mostafa@farsitel.com>
2011-01-11 19:04:48 +0800

    fix complie break for generic
    Change-Id: If5020d84f7fcb125faaf713c2ab2ba22795354ea

--- Android.mk
+++ Android.mk
@@ -203,7 +203,7 @@ endif
 
 # Added to build with the armv5te cflags changes.
 
-ifeq "$(findstring armv5te,$(TARGET_ARCH_VARIANT))" "armv5te"
+ifeq "$(findstring armv5te-vfp,$(TARGET_ARCH_VARIANT))" "armv5te-vfp"
         LOCAL_SRC_FILES += \
                 src/opts/S32_Opaque_D32_nofilter_DX_gether_arm.S
 endif
